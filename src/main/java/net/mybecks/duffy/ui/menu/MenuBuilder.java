a2169c79becd639e132b4b2e60055827d3a288a3
mybecks <andre_becker@gmx.net>
2014-05-30 14:53:11 +0200

    remove refresh menu, added accessibility contexts

--- src/main/java/net/mybecks/duffy/ui/menu/MenuBuilder.java
+++ src/main/java/net/mybecks/duffy/ui/menu/MenuBuilder.java
@@ -5,21 +5,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 
-import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import net.mybecks.duffy.ui.settings.SettingsBuilder;
 import net.mybecks.duffy.util.SettingsHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MenuBuilder implements ActionListener {
 	
@@ -44,7 +40,7 @@ public class MenuBuilder implements ActionListener {
 		menu = new JMenu("Menu");
 		menu.setMnemonic(KeyEvent.VK_A);
 		menu.getAccessibleContext().setAccessibleDescription(
-				"The only menu in this program that has menu items");
+				"Main Menu");
 		menuBar.add(menu);
 
 		// a group of JMenuItems
@@ -52,48 +48,26 @@ public class MenuBuilder implements ActionListener {
 		miPreferences.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1,
 				ActionEvent.ALT_MASK));
 		miPreferences.getAccessibleContext().setAccessibleDescription(
-				"This doesn't really do anything");
+				"Preferences");
 		miPreferences.addActionListener(this);
 		menu.add(miPreferences);
 		
 		miRefresh = new JMenuItem("Refresh", KeyEvent.VK_R);
 		miRefresh.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,
 				ActionEvent.ALT_MASK));
+		miRefresh.getAccessibleContext().setAccessibleDescription(
+				"Refresh the table");
 		miRefresh.addActionListener(this);
 		menu.add(miRefresh);
 		
-		
+		menu.addSeparator();
 		miQuit = new JMenuItem("Quit", KeyEvent.VK_Q);
 		miQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
 				ActionEvent.ALT_MASK));
+		miQuit.getAccessibleContext().setAccessibleDescription(
+				"Qunit the application");
 		miQuit.addActionListener(this);
 		menu.add(miQuit);
-		
-		menuRefresh = new JMenu("Refresh");
-		menuRefresh.setMnemonic(KeyEvent.VK_R);
-		menuRefresh.getAccessibleContext().setAccessibleDescription(
-				"The only menu in this program that has menu items");
-//		menuRefresh.addMenuListener(new MenuListener() {
-//			
-//			@Override
-//			public void menuSelected(MenuEvent e) {
-//				JOptionPane.showMessageDialog(null,
-//						"Refresh");
-//			}
-//			
-//			@Override
-//			public void menuDeselected(MenuEvent e) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			
-//			@Override
-//			public void menuCanceled(MenuEvent e) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		});;
-		menuBar.add(menuRefresh);
 		
 		
 		return menuBar;
@@ -114,5 +88,4 @@ public class MenuBuilder implements ActionListener {
 		}
 
 	}
-
 }
